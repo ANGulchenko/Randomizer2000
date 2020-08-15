b45f49c9f694e00fb6e7627bedace100d6a9c926
A.N.Gulchenko <a.n.gulchenko@gmail.com>
2020-08-15 16:27:48 +0300

    Should be enough for ZweiStein

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <vector>
+#include "Randomizer2000.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	cout << Randomizer2000::Instance().random(5, 20) << endl;
+	std::vector a = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	Randomizer2000::Instance().shuffle(a);
+	for (auto& i: a)
+	{
+		cout << i << " ";
+	}
+
+
 	return 0;
 }
